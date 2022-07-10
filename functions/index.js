d1f40cd1368d1d0dfe462b06741ac9dbb50aecb5
Kamasu Paul <kamasupaul@gmail.com>
2022-07-10 07:51:08 +0300

    -- add function for sending messages to a specific device

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./cheap-internal-sms.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cheap-internal-sms-app-default-rtdb.firebaseio.com",
+});
+
+// send data message to a device
+exports.sendMessage = functions.https.onRequest((req, res) => {
+  const registrationToken = req.query.registrationToken;
+  const phone = req.query.phone;
+  const message = req.query.message;
+  const payload = {
+    data: {
+      phone: phone,
+      message: message,
+    },
+  };
+  admin
+      .messaging()
+      .sendToDevice(registrationToken, payload)
+      .then((response) => {
+        console.log("Successfully sent message:", response);
+        res.send(response);
+      })
+      .catch((error) => {
+        console.log("Error sending message:", error);
+        res.send(error);
+      });
+});
